dbd6132328458d81b992767cfe447076e0bde522
ddongssazima <20242315@sdhs.gwe.hs.kr>
2024-11-18 15:21:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1NKIhMhUeRC0vPptHwT4it-LMYhamVDyi'
+file_id = '1HVKqM2mq-C1I1jPHlx5pNOSPfGYOH3_5'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
@@ -52,11 +52,11 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
     if prediction == labels[0]:
-         st.write("중냉 꿋굿")
+         st.write("중국냉면")
     elif prediction == labels[1]:
-         st.write("짜장면은 굿")
+         st.write("짜장면 뷁")
     elif prediction == labels[2]:
-         st.write("짬뽕은 맵지만 맛있어!!")
+         st.write("짬뽕은 싫어!!")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
